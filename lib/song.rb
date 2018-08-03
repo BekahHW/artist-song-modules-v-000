c2e4aafc5d8bcb8dc14979a34c5d2a87ad61340d
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-08-03 00:20:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,18 @@
 require 'pry'
 
 class Song
+  
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  
   attr_accessor :name
   attr_reader :artist
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  # def initialize
+  #   @@songs << self
+  # end
 
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
